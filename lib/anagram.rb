86b5017c3433dd3d963c7216deb6941903c363a2
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 17:43:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    char_array = @word.split
+    aoa_of_characters = array.map do |word|
+      word.split("")
+    end
+      if aoa_of_characters[i] - char_array = []
