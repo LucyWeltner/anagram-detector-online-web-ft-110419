315e0d0d33e70a4d62aa2cd8bc3ca63ed4ddd815
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 17:49:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,20 @@ class Anagram
   def match(array)
     anagrams = []
     char_array = @word.split
+    p char_array
     aoa_of_characters = array.map do |word|
       word.split("")
     end
+    p aoa_of_characters
     aoa_of_characters.each do |i|
       if i - char_array == []
         anagrams << i.join
+        p "adding #{i.join}"
       end
     end 
     anagrams
   end
 end
+
+anagram_maker = Anagram.new("fruits")
+anagram_maker.match(["toots", "strufi", "frutis", "banjo"])
